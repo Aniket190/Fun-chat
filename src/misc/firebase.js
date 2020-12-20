67c2bd44f77d504425c7c2775e547341ba99e009
Aniket <aniketkumar190@gmail.com>
2020-12-20 18:04:18 +0530

    Firebase Initialized

---
+++ src/misc/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+
+const config={
+    apiKey: "AIzaSyCEA828CT0m_XKuSKiK8Sp_V2WfAiXmi7A",
+    authDomain: "fun-chat-4a1d7.firebaseapp.com",
+    databaseURL: "https://fun-chat-4a1d7-default-rtdb.firebaseio.com",
+    projectId: "fun-chat-4a1d7",
+    storageBucket: "fun-chat-4a1d7.appspot.com",
+    messagingSenderId: "328693716107",
+    appId: "1:328693716107:web:c06638b0f5972749a4308e"
+  }
+
+const app=firebase.initializeApp(config);
