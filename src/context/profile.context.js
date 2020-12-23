501edb6d9283f0465c82976acd62f42fe9c97907
Aniket <aniketkumar190@gmail.com>
2020-12-23 13:39:30 +0530

    profile state management with contect

--- src/context/profile.context.js
+++ src/context/profile.context.js
@@ -1,11 +1,56 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { useEffect,createContext, useContext, useState } from 'react';
+import { auth, database } from '../misc/firebase';
 
 const ProfileContext=createContext();
 export const ProfileProvider = ({children}) => {
-    const [profile]=useState(false);
+    const [profile,setProfile]=useState(null);
+    const [isLoading,setisLoading]=useState(true)
+    useEffect(() => {
+        let userRef;
+
+        const authUnsub = auth.onAuthStateChanged( authObj  =>{
+
+            if(authObj){
+                  
+                userRef=database.ref(`/profiles/${authObj.uid}`);
+                userRef.on('value',snap=>{
+                        
+                    const {name,createdAt} = snap.val()
+                   
+                    const data= {
+                        name,
+                        createdAt,
+                        uid: authObj.uid,
+                        email:authObj.email
+                    }
+                    //  console.log(data); 
+                      setProfile(data);
+                      setisLoading(false);
+                 })
+
+            }else{
+                
+                if(userRef)
+                 userRef.off();
+
+                setProfile(null);
+                setisLoading(false);
+            }
+        })
+       return ()=>{
+          authUnsub();
+          if(userRef)
+           {
+             userRef.off();   
+           } 
+       } 
+    }, [])
+    
+    
+    
     return(
 
-  <ProfileContext.Provider value={profile}>
+  <ProfileContext.Provider value={{profile,isLoading}}>
       {children}
   </ProfileContext.Provider>
     )
