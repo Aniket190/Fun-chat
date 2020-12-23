fa19a066b4436cc86eeb05ef3ed93bc393257ef3
Aniket <aniketkumar190@gmail.com>
2020-12-23 17:32:56 +0530

    basic drawer inside Home

---
+++ src/components/Sidebar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import DashboardToggle from './dashboard/DashboardToggle';
+
+const Sidebar = () => {
+    return (
+        <div className="h-100 pt-2">
+            <div>
+                  
+                <DashboardToggle />
+
+
+                </div>
+
+                BOTTOM
+        </div>
+    )
+}
+
+export default Sidebar
