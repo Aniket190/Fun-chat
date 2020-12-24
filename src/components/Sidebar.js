5128a3f87da591dea9dc25021e5d9bbf0ef0fa98
Aniket <aniketkumar190@gmail.com>
2020-12-24 22:24:40 +0530

    created layout for home page and chat page

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,21 +1,29 @@
-import React from 'react'
-import CreateRoomBtnModal from './CreateRoomBtnModal';
+import React, { useRef, useState, useEffect } from 'react';
+import { Divider } from 'rsuite';
 import DashboardToggle from './dashboard/DashboardToggle';
+import CreateRoomBtnModal from './CreateRoomBtnModal';
+import ChatRoomList from './rooms/ChatRoomList';
 
 const Sidebar = () => {
-    return (
-        <div className="h-100 pt-2">
-            <div>
-                  
-                <DashboardToggle />
-                <CreateRoomBtnModal />
-                 
+  const topSidebarRef = useRef();
+  const [height, setHeight] = useState(0);
 
-                </div>
+  useEffect(() => {
+    if (topSidebarRef.current) {
+      setHeight(topSidebarRef.current.scrollHeight);
+    }
+  }, [topSidebarRef]);
 
-                BOTTOM
-        </div>
-    )
-}
+  return (
+    <div className="h-100 pt-2">
+      <div ref={topSidebarRef}>
+        <DashboardToggle />
+        <CreateRoomBtnModal />
+        <Divider>Join conversation</Divider>
+      </div>
+      <ChatRoomList aboveElHeight={height} />
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
