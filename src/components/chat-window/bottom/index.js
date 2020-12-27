6e4b3d072554580faf6d6f4f41211a90b9f18e00
Aniket <aniketkumar190@gmail.com>
2020-12-27 13:27:45 +0530

    Record button

--- src/components/chat-window/bottom/index.js
+++ src/components/chat-window/bottom/index.js
@@ -5,6 +5,7 @@ import { useParams } from 'react-router';
 import { useProfile } from '../../../context/profile.context';
 import { database } from '../../../misc/firebase';
 import AttachmentBtnModal from './AttachmentBtnModal';
+import AudioMsgBtn from './AudioMsgBtn';
 
 function assembleMessage(profile, chatId) {
   return {
@@ -105,7 +106,8 @@ const Bottom = () => {
     <div>
       <InputGroup>
         <AttachmentBtnModal afterUpload={afterUpload} />
-        <Input
+        <AudioMsgBtn afterUpload={afterUpload} />
+       <Input
           placeholder="Write a new message here..."
           value={input}
           onChange={onInputChange}
